a92ebefae5424e7c2d955d381ca878a11ad9e8de
이진향 <wlsgid2378@naver.com>
2020-09-15 19:55:53 +0900

    test

--- 웹크롤링/solved.ac스크레이핑/userdetail.py
+++ 웹크롤링/solved.ac스크레이핑/userdetail.py
@@ -3,46 +3,58 @@ from bs4 import BeautifulSoup
 import  urllib.request as req 
 from urllib.error import HTTPError,URLError
 import csv
+import pandas as pd
+import numpy as np
 
 # -*- coding:utf-8 -*-
-
-userid = "koosaga"
-url = "https://www.acmicpc.net/user/"+userid
+user = pd.read_csv('user.csv')
 
 
-f = open(userid+'.csv','w', encoding='utf-8-sig', newline='')
-f.write("정답여부, 문제 번호, 문제이름\n")
-wr = csv.writer(f)
-problemlist = []
-try:
-    headers = {'User-Agent':'Chrome/66.0.3359.181'}
-    rew = req.Request(url,headers=headers)
-    html = req.urlopen(rew)    
-except HTTPError as e:
-    err = e.read()
-    code = e.getcode()
-source = html.read()
-html.close()
-soup = BeautifulSoup(source,"html.parser")
-div1 = soup.select('.col-md-9')
-div2 = div1[0].select('.panel-body')
-div3 = div2[0].select('span')
-for i in range(0, len(div3),2):
-    list = []
-    list.append("true")
-    print(div3[i].text)
-    list.append(div3[i].text)
-    list.append(div3[i+1].text)
-    wr.writerow(list)
-
-div3 = div2[1].select('span')
-for i in range(0, len(div3),2):
-    list = []
-    list.append("false")
-    print(div3[i].text)
-    list.append(div3[i].text)
-    list.append(div3[i+1].text)
-    wr.writerow(list)
+for i in range(1000,2000):
+    users = user[user['등수']==i]
+    users.column
+    userlist = np.array(users['아이디'].tolist())
 
 
-f.close()
+
+
+def userdetail(userid):
+    url = "https://www.acmicpc.net/user/"+userid
+
+
+    f = open(userid+'.csv','w', encoding='utf-8-sig', newline='')
+    f.write("정답여부,문제번호,문제이름\n")
+    wr = csv.writer(f)
+    problemlist = []
+    try:
+        headers = {'User-Agent':'Chrome/66.0.3359.181'}
+        rew = req.Request(url,headers=headers)
+        html = req.urlopen(rew)    
+    except HTTPError as e:
+        err = e.read()
+        code = e.getcode()
+    source = html.read()
+    html.close()
+    soup = BeautifulSoup(source,"html.parser")
+    div1 = soup.select('.col-md-9')
+    div2 = div1[0].select('.panel-body')
+    div3 = div2[0].select('span')
+    for i in range(0, len(div3),2):
+        list = []
+        list.append("true")
+        print(div3[i].text)
+        list.append(div3[i].text)
+        list.append(div3[i+1].text)
+        wr.writerow(list)
+
+    div3 = div2[1].select('span')
+    for i in range(0, len(div3),2):
+        list = []
+        list.append("false")
+        print(div3[i].text)
+        list.append(div3[i].text)
+        list.append(div3[i+1].text)
+        wr.writerow(list)
+
+
+    f.close()
